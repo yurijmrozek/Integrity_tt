7dca3c925dabd6088acf209a85e9506ce90000e5
Yurij Mrozek <69200681+yurijmrozek@users.noreply.github.com>
2021-07-26 04:21:39 +0300

    Update ApiControllerTest.java

--- src/test/java/world/ucode/ApiControllerTest.java
+++ src/test/java/world/ucode/ApiControllerTest.java
@@ -1,8 +1,6 @@
 package world.ucode;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
