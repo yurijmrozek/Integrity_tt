e8d7a12e224938ab8fa48618cab0d5141897fb79
Yurij Mrozek <69200681+yurijmrozek@users.noreply.github.com>
2021-07-26 04:22:09 +0300

    Update Application.java

--- src/main/java/world/ucode/Application.java
+++ src/main/java/world/ucode/Application.java
@@ -2,7 +2,6 @@ package world.ucode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.server.LocalServerPort;
 
 @SpringBootApplication
 public class Application {
